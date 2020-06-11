3a749556e5b210d1060d7d2e80d85dbf9880d74c
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-11 04:33:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,9 @@ class Transfer
   # your code here
   attr_accessor :sender, :receiver, :amount
   def initialize(sender, receiver, amount)
-
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
   end
 
 end
