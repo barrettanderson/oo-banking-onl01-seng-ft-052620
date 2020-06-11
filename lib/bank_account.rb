735571da09edcd73aa15a5c870f4bbc39b1d34ff
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-11 04:39:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
-  def initialize(name, status="open")
+  def initialize(name, balance=1000, status="open")
     @name = name
-    @balance = 1000
+    @balance = balance
     @status = status
   end
 
